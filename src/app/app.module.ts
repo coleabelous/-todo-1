684692bf310ac387867d1b102d745da41d209218
Colea <colea.belous@gmail.com>
2021-07-13 11:50:04 +0300

    добавили компонент и сервис categories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
+import {CategotiesComponent} from './views/categoties/categoties.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    CategotiesComponent,
 
   ],
   imports: [
